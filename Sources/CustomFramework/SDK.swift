d815e431480b29185a68835955e418d12b1eec01
Felipe Loyola <felipe.loyola@externos.bci.cl>
2023-04-01 18:27:03 -0300

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  SDK.swift
+//  
+//
+//  Created by Felipe Loyola on 01-04-23.
+//
+
+import Foundation
+
+
+public struct SDK {
+    static func doSomeWork() {
+        print("hola mundo")
+    }
+}
